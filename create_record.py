225adc895cc67d4b75c18676969d2464d2f6cd79
Petar Pavlovic <pero356@gmail.com>
2020-09-21 15:05:04 +0200

    [add] create_record

--- create_record.py
+++ create_record.py
@@ -1,25 +1,34 @@
-from lib.loader_coco.RecordReader import RecordReader
-from lib.loader_coco.RecordWriter import RecordWriter
-from gluoncv import utils
+from lib.loader.RecordReader import RecordReader
+from lib.loader.RecordWriter import RecordWriter
 from IPython import embed
 import matplotlib
 matplotlib.use('tkagg')
 from matplotlib import pyplot as plt
 import numpy as np
 import json
-from lib.loader_coco.classes import classes
+from lib.split.split import Split
+
+
+split = Split(
+        images_dir='/media/david/A/Datasets/PlayHippo/images',
+        masks_dir='/media/david/A/Datasets/PlayHippo/masked_images',
+    )
+
+train_set, test_set = split.create_splits()
 
 writer = RecordWriter(
-    data_path           = '/media/david/A/Dataset/COCO',
+    data_path           = '/media/david/A/Dataset/PlayHippo',
     record_dir          = 'records',
-    record_name         = 'one_image',
+    record_name         = 'data',
+    train_set           = train_set,
+    test_set            = test_set,
     save_n_test_images  = 1,
     save_n_train_images = 1,
 )
 
 reader = RecordReader(
     record_dir           = 'records',
-    record_name          = 'one_image',
+    record_name          = 'data',
     batch_size           = 1,
     shuffle_buffer       = 1,
     num_parallel_calls   = 1,
@@ -28,15 +37,48 @@ reader = RecordReader(
     count                = 1,
 )
 
-test_record = reader.read_record('test')
+test_record = reader.read_record('train')
 
-for index, class_ids, bboxes, image in test_record:
+def show(image):
+    import cv2
+    cv2.imshow('', image)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
+def fillhole(input_image):
+    '''
+    input gray binary image  get the filled image by floodfill method
+    Note: only holes surrounded in the connected regions will be filled.
+    :param input_image:
+    :return:
+    '''
+    im_flood_fill = input_image.copy()
+    h, w = input_image.shape[:2]
+    mask = np.zeros((h + 2, w + 2), np.uint8)
+    im_flood_fill = im_flood_fill.astype("uint8")
+    cv2.floodFill(im_flood_fill, mask, (0, 0), 255)
+    im_flood_fill_inv = cv2.bitwise_not(im_flood_fill)
+    img_out = input_image | im_flood_fill_inv
+    return img_out
+
+
+for name, image, mask in test_record:
+    import cv2
     image = (image[0].numpy() * 255).astype(np.uint8)
+    mask = (mask[0].numpy() * 255).astype(np.uint8)
 
-    bboxes = bboxes.numpy()
+    print (name)
 
-    class_ids = class_ids.numpy()
+    # mask_of_mask = np.ones_like(mask)
+    # mask_of_mask *= 255
+    # mask_of_mask[mask > 10] = 0
 
-    utils.viz.plot_bbox(image, bboxes, scores=None, labels=class_ids, class_names=list(classes.values()))
-    plt.show()
+    show(mask)
+
+    m = mask.copy()
+    m = m.astype(np.bool)
+    m = m.astype(np.uint8) * 255
+
+    show(m)
+
+    show(fillhole(m[:, :, 0]))
